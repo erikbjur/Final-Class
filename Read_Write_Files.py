529c27855a1b71720430eba8bfe2adb80c63281c
Erik Bjur <erik@bjurengineering.com>
2023-12-14 12:25:05 -0800

    Finished program

--- Read_Write_Files.py
+++ Read_Write_Files.py
@@ -1,10 +1,35 @@
 names = ["Fred", "Bob", "Chuck"]
-ages = [56, 34, 23]
 
-reader = open('TestFile.txt', "x")
-reader.write("Names")
+file = open('TestFile.txt', 'w')
+
 try:
     for value in names:
-        reader.write(value + '\n')
+        file.write(value + '\n')
 finally:
-    reader.close()
+    file.close()
+
+# lets make a class to hold the person data in
+class Person:
+    def __init__(self, nameOfPerson) -> None:
+        self.Name = nameOfPerson
+    
+    def PrintName(self) -> None:
+        print(f"{self.Name} is my name!")
+
+# now lets create a list of people from the list
+listOfPeople = []
+
+file = open('TestFile.txt', "r")
+
+try:
+    for line in file:
+        # create a person with this name
+        newPerson = Person(line.rstrip())
+        # add them to the list of people
+        listOfPeople.append(newPerson)
+finally:
+    file.close()
+
+# now print off the names of the people in the list
+for person in listOfPeople:
+    person.PrintName()
