b56d295b0ec1e4ce1928e2996b2417e82c8147b8
Erik Bjur <erik@bjurengineering.com>
2023-12-14 12:29:10 -0800

    Added comments

--- Read_Write_Files.py
+++ Read_Write_Files.py
@@ -1,9 +1,15 @@
+# here is a list of names
 names = ["Fred", "Bob", "Chuck"]
 
+# here we open the file as write only
 file = open('TestFile.txt', 'w')
 
+# here we try to open the file, and it it works
+# we write all the data to the file
 try:
+    # loop through the list of names
     for value in names:
+        # write each name to the file with a new line character
         file.write(value + '\n')
 finally:
     file.close()
@@ -19,6 +25,7 @@ class Person:
 # now lets create a list of people from the list
 listOfPeople = []
 
+# here we open the file as read only
 file = open('TestFile.txt', "r")
 
 try:
@@ -28,6 +35,7 @@ try:
         # add them to the list of people
         listOfPeople.append(newPerson)
 finally:
+    # here's how we close the file
     file.close()
 
 # now print off the names of the people in the list
