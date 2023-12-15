936ea0776f3c7d614e0dd30d9acb66e47d936d8d
Erik Bjur <erik@bjurengineering.com>
2023-12-14 18:45:30 -0800

    Finished adding comments

--- Read_Write_Files.py
+++ Read_Write_Files.py
@@ -2,33 +2,40 @@
 names = ["Fred", "Bob", "Chuck"]
 
 # here we open the file as write only
-file = open('TestFile.txt', 'w')
+file = open('TestFile.rad', 'w') # the 'w' means write
 
-# here we try to open the file, and it it works
+# here we try to open the file, and if it works
 # we write all the data to the file
 try:
     # loop through the list of names
     for value in names:
         # write each name to the file with a new line character
-        file.write(value + '\n')
+        file.write(value + '\n') # note that the '\n' is the end line character
 finally:
+    # if the file was opened and the data written,
+    # then close the file
     file.close()
 
+# now lets do something with the data we wrote to the file
 # lets make a class to hold the person data in
 class Person:
+    # here's the constructor for the person class
     def __init__(self, nameOfPerson) -> None:
         self.Name = nameOfPerson
     
+    # here's a method to print the name of the person
     def PrintName(self) -> None:
         print(f"{self.Name} is my name!")
 
-# now lets create a list of people from the list
+# now lets create a list of people
+# we create from the data in the file
 listOfPeople = []
 
 # here we open the file as read only
-file = open('TestFile.txt', "r")
+file = open('TestFile.rad', 'r') # the 'r' means read only
 
 try:
+    # loop through each line in the file
     for line in file:
         # create a person with this name
         newPerson = Person(line.rstrip())
